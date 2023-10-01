4ecddb5985710391f57de972dac5448b54988395
Erick Barrera <dannbj1@gmail.com>
2023-09-30 23:24:02 -0600

    Creación clases

---
+++ EntradaDatos.java
@@ -0,0 +1,110 @@
+
+import java.util.Scanner;
+
+public class EntradaDatos {
+    private Scanner scan;
+    public EntradaDatos(Scanner scan){this.scan = scan;}
+    public int pedirTipo(){
+        System.out.println("Ingrese el número de la observación que observo: "+
+                                    "\n1. Estrellas"+
+                                    "\n2. Planeta"+
+                                    "\n3. Galaxia"+
+                                    "\n4. Nebulos"+
+                                    "\n5. Cometa"+
+                                    "\n6. Asteroide");
+        String opcString = scan.nextLine().trim().toLowerCase();
+        int opcion = 0;
+        try {
+            opcion = Integer.parseInt(opcString);
+            return opcion;
+        } catch (NullPointerException | NumberFormatException e) {
+            System.out.println("Ingrese una opción válida!");
+            pedirTipo();
+        }
+        return opcion;
+    }
+
+    public String nombreAstronomo(){
+        System.out.println("Ingrese el nombre del astrónomo: ");
+        String name = scan.nextLine();
+        name = name.trim().toLowerCase();        
+        return name;
+    }
+
+    public String fechaObservacion(){
+        System.out.println("Ingrese la fecha de la observación con el siguiente formato (dd/mm/aaaa): ");
+        String fecha = scan.nextLine();
+        fecha = fecha.trim().toLowerCase();
+        return fecha;
+    }
+
+    public float magnitud(){
+        System.out.println("Ingrese la magnitud aparente de la observación: ");
+        try {
+            float magnitud = scan.nextFloat();
+            scan.nextLine();
+            return magnitud;
+        } catch (Exception e) {
+            System.out.println("ingrese un valor válido");
+            magnitud();
+        }
+        float magnitud = 0;
+        return magnitud;
+    }
+
+    public String telescopio(){
+        System.out.println("Ingrese el nombre del telescopio utilizado para la observación: ");
+        String telescope = scan.nextLine();
+        telescope = telescope.trim().toLowerCase();
+        return telescope;
+    }
+
+    public String tipoEspectral(){
+        System.out.println("Seleccione el tipo espectral de la estrella: "+
+                                "\n1.O"+
+                                "\n2.B"+
+                                "\n3.A"+
+                                "\n4.F"+
+                                "\n5.G"+
+                                "\n6.K"+
+                                "\n7.M");
+        try {
+            int opcion = scan.nextInt();
+            scan.nextLine();
+            switch (opcion) {
+                case 1:
+                    return "O";
+                    
+                case 2:
+                    return "B";
+                    
+                case 3:
+                    return "A";
+                    
+                case 4:
+                    return "F";
+                    
+                case 5: 
+                    return "G";
+                    
+                case 6:
+                    return "K";
+                    
+                case 7: 
+                    return "M";
+            
+                default:
+                    System.out.println("Ingrese una opcion valida");
+                    pedirTipo();
+            }
+        } catch (Exception e) {
+            System.out.println("Ingrese un valor válido!");
+            tipoEspectral();
+        }
+        return "";
+    }
+
+    public int[] coordenadas(){
+        
+    }
+}
--- Observacion.java
+++ Observacion.java
@@ -1,3 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Observacion {
-    
+    private Scanner scan;
+    private EntradaDatos entrada = new EntradaDatos(scan);
+    private ArrayList<ObjetoCeleste> objetosObservados;
+    private ArrayList<Estrella> estrellasObservadas;
+    private ArrayList<Galaxia> galaxiasObservadas;
+
+    public Observacion(Scanner scan, ArrayList<ObjetoCeleste> objetosObseravdos, ArrayList<Estrella> estrellasObservadas, ArrayList<Galaxia> galaxiasObservadas){
+        this.scan = scan;
+        this.objetosObservados = objetosObseravdos;
+        this.estrellasObservadas = estrellasObservadas;
+        this.galaxiasObservadas = galaxiasObservadas;
+    }
+
+
+
+
+    public void registrarObservacion(){
+        switch (entrada.pedirTipo()) {
+            case 1:
+                estrellasObservadas.add(
+                    new Estrella(entrada.nombreAstronomo(), entrada.fechaObservacion(), entrada.magnitud(), "Estrella",
+                    entrada.telescopio(), null, entrada.tipoEspectral())
+                );
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+    public void listaEstrellas(){}
+
+    public void cantidadEstrellas(){}
+
+    public void mostrarObservaciones(){}
+
+    public void galaxiasTipo(){}
 }
