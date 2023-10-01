ffc3870c089f87f0e4a42df400da1bce576641c1
Estebancin <esteban1110rp@gmail.com>
2023-09-30 19:58:01 -0600

    Creación clase Galaxia

---
+++ Galaxia.java
@@ -0,0 +1,26 @@
+public class Galaxia extends ObjetoCeleste {
+    private String tipo;
+    private int distancia;
+
+    public Galaxia(String nombre, String fecha, float magnitudAparente, String tipoObjetoCeleste, String telescopio, int[] coordenadas, String tipoGalaxia, int distancia) {
+        super(nombre, fecha, magnitudAparente, tipoObjetoCeleste, telescopio, coordenadas);
+        this.tipo = tipoGalaxia;
+        this.distancia = distancia;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(int distancia) {
+        this.distancia = distancia;
+    }
+}
